c15d2dbe4baa0ccbc3cb9bbfede16ddbdc929984
Ismail910 <ismailmagdy566@gmail.com>
2023-05-29 11:49:09 +0300

    add

--- front_school/src/app/components/teacher/student/student.component.ts
+++ front_school/src/app/components/teacher/student/student.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ApiService }  from 'src/app/services/student/student.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit{
+  students!:any;
+ 
+ 
+  constructor(private Api: ApiService, private http: HttpClient){
 
+  }
+  ngOnInit(): void {
+   this.getBooks();
+  }
+
+  
+  getBooks()
+    {
+      this.Api.get(`${environment.baseUrl}/home/all/page/${this.page}`).subscribe(data=>{
+        this.students=data.data;
+       
+     
+        this.totalPages=data.pages.totalPages;
+        this._pagination=[...Array(this.totalPages).keys()];
+      })
+    }
 }
--- front_school/src/app/services/student/student.service.ts
+++ front_school/src/app/services/student/student.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class StudendService {
 
-  constructor() { }
+
+
+
+  
+  constructor( private Http:HttpClient ){}
+
+
+// getBooksByStatus(status:string,userID:string):Observable<any>{
+//   return this.Http.get<Book[]>(`${environment.baseUrl}/home/page/1/?${status}?${userID}` ,{
+//     headers: new HttpHeaders().set('Authorization','secrt token')
+//   })
+// }
+
+  getAllstudents():Observable<any>{
+    return this.Http.get<any>(`${environment.baseUrl}/student`, {
+    headers : new HttpHeaders().set('Authorization', 'secrt token')
+    })
+  }
+
+
+  getstudent(studentId:string):Observable<any>{
+      return this.Http.get<any>(`${environment.baseUrl}/student/${studentId}`)
+    }
+
+  
+  
+
 }
+
+
+
+
